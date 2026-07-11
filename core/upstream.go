fddf3cafff25de1645f4c82f884dade1f45d1a6b
Jordyn <onlinecloud1@gmail.com>
2026-07-10 21:13:02 -0400

    Refactor ConnectToServer to use a map for upstream handler registration

--- core/upstream.go
+++ core/upstream.go
@@ -1,72 +1,88 @@
 // core/upstream.go
-// This file contains the core of the Bouncer structure as it relates to the upstream connection
 package core
 
 import (
 	"bouncer/upstreamHandlers"
 
-	"github.com/rs/zerolog/log"
-
 	"github.com/ergochat/irc-go/ircevent"
+	"github.com/ergochat/irc-go/ircmsg"
+	"github.com/rs/zerolog/log"
 )
 
 func (b *Bouncer) ConnectToServer(conn *ircevent.Connection) (err error) {
 	// Store the upstream connection for later use
-	// Update: this is the cleanest way to do it
 	b.upstreamConn = conn
 
-	// Register upstream handlers
-	// These are commands send by the connected IRC servers
-	upstreamHandlers.Register(b, "PING", upstreamHandlers.HandlePING)
-	upstreamHandlers.Register(b, "PRIVMSG", upstreamHandlers.HandlePRIVMSG)
-	upstreamHandlers.Register(b, "NOTICE", upstreamHandlers.HandleNOTICE)
-	upstreamHandlers.Register(b, "NICK", upstreamHandlers.HandleNICK)
-	upstreamHandlers.Register(b, "332", upstreamHandlers.Handle332) // /TOPIC on join
-	upstreamHandlers.Register(b, "353", upstreamHandlers.Handle353) // /NAMES
-	upstreamHandlers.Register(b, "366", upstreamHandlers.Handle366) // End of /NAMES
-	upstreamHandlers.Register(b, "JOIN", upstreamHandlers.HandleJOIN)
-	upstreamHandlers.Register(b, "TOPIC", upstreamHandlers.HandleTOPIC)
-	upstreamHandlers.Register(b, "KICK", upstreamHandlers.HandleKICK)
-	upstreamHandlers.Register(b, "MODE", upstreamHandlers.HandleMODE)
-	upstreamHandlers.Register(b, "474", upstreamHandlers.Handle474) // Cannot join channel (+b)
-	upstreamHandlers.Register(b, "329", upstreamHandlers.Handle329) // Channel creation timestamp
-	upstreamHandlers.Register(b, "473", upstreamHandlers.Handle473) // Cannot join channel (+i)
-	upstreamHandlers.Register(b, "PART", upstreamHandlers.HandlePART)
-	upstreamHandlers.Register(b, "001", upstreamHandlers.Handle001) // RPL_WELCOME
-	upstreamHandlers.Register(b, "477", upstreamHandlers.Handle477) // Cannot join channel (unregistered)
-	upstreamHandlers.Register(b, "900", upstreamHandlers.Handle900) // RPL_LOGGEDIN
-	upstreamHandlers.Register(b, "302", upstreamHandlers.Handle302) // RPL_USERHOST
+	// Map of IRC commands to their handler functions
+	// If upstreamHandlers exports a specific type (e.g., upstreamHandlers.Handler),
+	// you can replace the func signature below with that type.
+	handlers := map[string]func(upstreamHandlers.Router, ircmsg.Message) error{
+		// Basic messaging and connection
+		"PING":    upstreamHandlers.HandlePING,
+		"PRIVMSG": upstreamHandlers.HandlePRIVMSG,
+		"NOTICE":  upstreamHandlers.HandleNOTICE,
+		"NICK":    upstreamHandlers.HandleNICK,
+		"QUIT":    upstreamHandlers.HandleQUIT,
 
-	upstreamHandlers.Register(b, "324", upstreamHandlers.Handle324) // Channel modes (e.g. +Cnst)
-	upstreamHandlers.Register(b, "367", upstreamHandlers.Handle367) // Banlist item
-	upstreamHandlers.Register(b, "368", upstreamHandlers.Handle368) // End of banlist
-	upstreamHandlers.Register(b, "421", upstreamHandlers.Handle421) // RPL_UNKNOWNCOMMAND
+		// Channel operations
+		"JOIN":  upstreamHandlers.HandleJOIN,
+		"PART":  upstreamHandlers.HandlePART,
+		"TOPIC": upstreamHandlers.HandleTOPIC,
+		"KICK":  upstreamHandlers.HandleKICK,
+		"MODE":  upstreamHandlers.HandleMODE,
 
-	// WHOIS related handlers
-	upstreamHandlers.Register(b, "311", upstreamHandlers.Handle311)
-	upstreamHandlers.Register(b, "319", upstreamHandlers.Handle319)
-	upstreamHandlers.Register(b, "312", upstreamHandlers.Handle312)
-	upstreamHandlers.Register(b, "671", upstreamHandlers.Handle671)
-	upstreamHandlers.Register(b, "338", upstreamHandlers.Handle338)
-	upstreamHandlers.Register(b, "317", upstreamHandlers.Handle317)
-	upstreamHandlers.Register(b, "318", upstreamHandlers.Handle318)
-	upstreamHandlers.Register(b, "330", upstreamHandlers.Handle330)
-	upstreamHandlers.Register(b, "402", upstreamHandlers.Handle402)
+		// Numerics: Connection & Welcome
+		"001": upstreamHandlers.Handle001, // RPL_WELCOME
+		"900": upstreamHandlers.Handle900, // RPL_LOGGEDIN
 
-	upstreamHandlers.Register(b, "QUIT", upstreamHandlers.HandleQUIT)
+		// Numerics: Channel state & lists
+		"324": upstreamHandlers.Handle324, // Channel modes
+		"329": upstreamHandlers.Handle329, // Channel creation timestamp
+		"332": upstreamHandlers.Handle332, // Topic on join
+		"353": upstreamHandlers.Handle353, // NAMES
+		"366": upstreamHandlers.Handle366, // End of NAMES
+		"367": upstreamHandlers.Handle367, // Banlist item
+		"368": upstreamHandlers.Handle368, // End of banlist
 
-	// MOTD
-	upstreamHandlers.Register(b, "375", upstreamHandlers.Handle375) // MOTD Start
-	upstreamHandlers.Register(b, "372", upstreamHandlers.Handle372) // MOTD line
-	upstreamHandlers.Register(b, "376", upstreamHandlers.Handle376) // MOTD End
-	upstreamHandlers.Register(b, "422", upstreamHandlers.Handle422) // MOTD missing
+		// Numerics: Errors & Restrictions
+		"421": upstreamHandlers.Handle421, // RPL_UNKNOWNCOMMAND
+		"473": upstreamHandlers.Handle473, // Cannot join channel (+i)
+		"474": upstreamHandlers.Handle474, // Cannot join channel (+b)
+		"477": upstreamHandlers.Handle477, // Cannot join channel (unregistered)
 
-	// AWAY related handlers
-	upstreamHandlers.Register(b, "305", upstreamHandlers.Handle305) // RPL_305 No Longer Away
-	upstreamHandlers.Register(b, "306", upstreamHandlers.Handle306) // RPL_306 Client Away
+		// Numerics: WHOIS
+		"311": upstreamHandlers.Handle311,
+		"312": upstreamHandlers.Handle312,
+		"317": upstreamHandlers.Handle317,
+		"318": upstreamHandlers.Handle318,
+		"319": upstreamHandlers.Handle319,
+		"330": upstreamHandlers.Handle330,
+		"338": upstreamHandlers.Handle338,
+		"402": upstreamHandlers.Handle402,
+		"671": upstreamHandlers.Handle671,
 
-	// Identification related handlers (vhost, nickserv, etc)
-	upstreamHandlers.Register(b, "396", upstreamHandlers.Handle396)
+		// Numerics: WHO
+		"352": upstreamHandlers.Handle352, // WHO line
+		"315": upstreamHandlers.Handle315, // end of who
+
+		// Numerics: MOTD
+		"372": upstreamHandlers.Handle372, // MOTD line
+		"375": upstreamHandlers.Handle375, // MOTD Start
+		"376": upstreamHandlers.Handle376, // MOTD End
+		"422": upstreamHandlers.Handle422, // MOTD missing
+
+		// Numerics: Away & Identification
+		"302": upstreamHandlers.Handle302, // RPL_USERHOST
+		"305": upstreamHandlers.Handle305, // RPL_305 No Longer Away
+		"306": upstreamHandlers.Handle306, // RPL_306 Client Away
+		"396": upstreamHandlers.Handle396, // Vhost / Nickserv
+	}
+
+	// Register all upstream handlers dynamically
+	for cmd, handler := range handlers {
+		log.Trace().Msgf("[bouncer] Registering upstream handler for %s", cmd)
+		upstreamHandlers.Register(b, cmd, handler)
+	}
 
 	// Connect to the upstream server
 	err = conn.Connect()
