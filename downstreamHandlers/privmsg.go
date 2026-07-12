d379c95b3c774cc3a625a28c9cb93aa295284d63
Jordyn <onlinecloud1@gmail.com>
2026-07-12 00:30:46 -0400

    Fix downstream PRIVMSG handler to log outgoing messages

--- downstreamHandlers/privmsg.go
+++ downstreamHandlers/privmsg.go
@@ -2,6 +2,7 @@ package downstreamHandlers
 
 import (
 	"github.com/ergochat/irc-go/ircmsg"
+	"github.com/rs/zerolog/log"
 
 	// Replace "yourmodule" with the module name defined in your go.mod
 	"bouncer/core"
@@ -12,6 +13,21 @@ func HandlePRIVMSG(b *core.Bouncer, ds *core.DownstreamConnection, msg ircmsg.Me
 	// Ignore blank messages
 	if len(msg.Params) <= 0 {
 		return nil
+	}
+
+	// Log to database
+	if msg.Source == "" {
+		log.Debug().Msgf("[downstream %s] Adding downstream nick to PRIVMSG command for log", ds.Conn.RemoteAddr())
+		msg.Source = ds.Nick
+		b.LogToDB(msg)
+		msg.Source = ""
+	} else {
+		// I'm not sure if this is strictly *necessary* as I don't think IRC allows
+		// clients to specify an arbitrary prefix.
+		//
+		// But better be safe than sorry.
+		log.Debug().Msgf("[downstream %s] Downstream PRIVMSG already has source, logging bare", ds.Conn.RemoteAddr())
+		b.LogToDB(msg)
 	}
 
 	// Handle bouncer commands
